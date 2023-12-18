6ac80f21874a5cb73417325193c29ec6dcc453f2
Med Zied BEN GHORBAL <ziedbenghorbel@gmail.com>
2023-12-18 22:04:50 +0100

    Initial commit

---
+++ src/main/java/fr/ileadconsulting/condinggameback/CondingGameBackApplication.java
@@ -0,0 +1,13 @@
+package fr.ileadconsulting.condinggameback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondingGameBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondingGameBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ileadconsulting/condinggameback/CondingGameBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ileadconsulting.condinggameback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondingGameBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
